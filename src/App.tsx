b4f42a1ce305265c3de5087e0ec31a37c6f3f866
Jesus24-Dev <siritjesus24@gmail.com>
2025-03-24 12:10:48 -0400

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Routes, Route } from "react-router-dom"
+import { Home } from "./pages/Home"
+import { Match } from "./pages/Match"
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/match" element={<Match/>}/>
+    </Routes>
   )
 }
 
