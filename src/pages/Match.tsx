dba8e6dde98f712f7f6976e46c5305a79135b716
Jesus24-Dev <siritjesus24@gmail.com>
2025-03-27 16:07:59 -0400

    bind BingoCard and BingoHost components

--- src/pages/Match.tsx
+++ src/pages/Match.tsx
@@ -1,11 +1,27 @@
-import { BingoCard } from "../components/BingoCard";
+import { useState } from 'react';
+import { BingoHost } from '../components/BingoHost';
+import { BingoCard } from '../components/BingoCard';
+import { BingoNumber } from '../types/bingo';
 
-export function Match(){
+export function Match() {
+    const [calledNumbers, setCalledNumbers] = useState<BingoNumber[]>([]);
+
+    const handleNumberCalled = (bingoNumber: BingoNumber) => {
+        setCalledNumbers(prev => [...prev, bingoNumber]);
+    };
+
     return (
-        <div>
-            <h1>Match page</h1>
-            <BingoCard/>
+      <div className="min-h-screen bg-gray-100 p-4">
+        <div className="max-w-6xl mx-auto grid grid-cols-1 lg:grid-cols-3 gap-8">
+          <div className="lg:col-span-2">
+            <BingoHost 
+              onNumberCalled={handleNumberCalled}
+            />
+          </div>
+          <div className="flex justify-center">
+            <BingoCard calledNumbers={calledNumbers} />
+          </div>
         </div>
+      </div>
     );
-};
-
+}
