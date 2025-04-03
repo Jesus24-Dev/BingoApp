4330fd0f0cd6c69011e782d505497f1255e99d10
Jesus24-Dev <siritjesus24@gmail.com>
2025-04-03 17:40:46 -0400

    created hook for socket connection;

---
+++ src/hooks/useSocket.ts
@@ -0,0 +1,18 @@
+import { io, Socket } from 'socket.io-client';
+import { useEffect, useState } from 'react';
+
+export const useSocket = (url: string) => {
+  const [socket, setSocket] = useState<Socket | null>(null);
+
+  useEffect(() => {
+    const socketIo = io(url);
+
+    setSocket(socketIo);
+
+    return () => {
+      socketIo.disconnect();
+    };
+  }, [url]);
+
+  return socket;
+};
