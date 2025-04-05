9900009fb085b0f1cc9c129f2681aa3bba5b9779
Jesus24-Dev <siritjesus24@gmail.com>
2025-04-05 19:46:59 -0400

    Add function to show uniqueWinners, add map for show winners list

--- src/components/StartingPanel/BingoHost/WinnerBanner.tsx
+++ src/components/StartingPanel/BingoHost/WinnerBanner.tsx
@@ -1,17 +1,28 @@
 import { BingoWinner } from "../../../types/bingo";
 
 interface WinnerBannerProps {
-    winner: BingoWinner | null; 
+    winner: BingoWinner[] | null; 
 }
 
 const WinnerBanner = ({ winner }: WinnerBannerProps) => {
     if (!winner) return null;
+ 
+    const uniqueWinners = winner.filter(
+        (winner, index, self) => index === self.findIndex(w => w.winner.playerId === winner.winner.playerId)
+      );
+
+    winner.map((w) => {
+        console.log("Ganador: ", w.winner.playerName, "Patrón: ", w.winner.pattern);
+    })
+
     return (
         <div className="bg-gradient-to-r from-yellow-400 to-yellow-500 text-white text-center p-4 mb-6 rounded-lg shadow-lg animate-pulse">
             <h2 className="text-2xl font-bold">🎉 ¡BINGO! 🎉</h2>
-            <p className="text-lg">
-                {winner.playerName} ha completado: {winner.pattern}
-            </p>
+            {uniqueWinners.map((w, index) => (
+                <p key={index} className="text-lg">
+                    {w.winner.playerName} ha ganado con el patrón: {w.winner.pattern}
+                </p>
+            ))}
         </div>
     );
 };
